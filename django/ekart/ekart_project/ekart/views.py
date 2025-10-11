a971fa1970c2495ad80678fe58235d04cfcdb657
ameen-git9 <ameenanu890@gmail.com>
2025-10-11 12:27:08 +0530

    added templates and static files

--- django/ekart/ekart_project/ekart/views.py
+++ django/ekart/ekart_project/ekart/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name='index.html'
