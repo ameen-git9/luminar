25c4e1d6fcf15ee229aa66ec33f0f823071b4946
ameen-git9 <ameenanu890@gmail.com>
2025-10-13 22:28:44 +0530

    register and login created.

--- django/ekart/ekart_project/ekart/views.py
+++ django/ekart/ekart_project/ekart/views.py
@@ -1,9 +1,66 @@
 from django.shortcuts import render,redirect
 from django.views import View
 from django.views.generic import TemplateView
+from ekart.models import Product
+from django.contrib.auth.models import User
+from ekart.forms import UserRegisterForm,UserLogin
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 # Create your views here.
 
 
-class HomeView(TemplateView):
-    template_name='index.html'
+class HomeView(View):
+    def get(self,request):
+        product=Product.objects.all()
+        return render(request,'index.html',{'product':product})
+    
+
+
+
+
+class DetailsView(View):
+    def get(self,request,**kwargs):
+        id=kwargs.get("id")
+        prod=Product.objects.get(id=id)
+        return render(request, 'productview.html',{'product':prod})
+
+
+
+
+
+class UserRegisterView(View):
+    def get(self,request):
+        form=UserRegisterForm()
+        return render(request,'register.html',{'form':form})
+    def post(self,request):
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(**form.cleaned_data)
+            messages.success(request,"registration successfull")
+            return redirect('userloginview')
+        else:
+            messages.warning(request,"invalid  uusername or password")
+            return redirect('registerview')
+        
+
+
+
+
+class UserLoginView(View):
+    def get(self,request):
+        form=UserLogin()
+        return render(request,'userlogin.html',{'form':form})
+    
+    def post(self,request):
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        res=authenticate(request,username=username,password=password)
+        if res:
+            login(request,res)
+            messages.success(request,"successfull")
+            return redirect('homeview')
+        else:
+            messages.warning(request,"invalid username or password")
+            return redirect('userlogview')
--- django/ekart/ekart_project/ekart_project/urls.py
+++ django/ekart/ekart_project/ekart_project/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ekart import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.HomeView.as_view(),name="homeview")
-]
+    path('',views.HomeView.as_view(),name="homeview"),
+    path('detail/<int:id>',views.DetailsView.as_view(),name="detailview"),
+    path('reg',views.UserRegisterView.as_view(),name="registerview"),
+    path('log',views.UserLoginView.as_view(),name="userloginview"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
