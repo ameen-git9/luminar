f643c0ffe0e1b50b30f45dad93e734efb6a83904
ameen-git9 <ameenanu890@gmail.com>
2025-10-21 12:48:54 +0530

    instructor register form created. the the rolr assigned instructor

--- django/e-learning/learnproject/instructorapp/views.py
+++ django/e-learning/learnproject/instructorapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from instructorapp.forms import InstructorCreateForm
+from instructorapp.models import User
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+
+class InstructorView(View):
+    def get(self,request):
+        form=InstructorCreateForm()
+        return render(request,'instructor_reg.html',{'form':form})
+    
+
+    def post(self,request):
+        form_instance=InstructorCreateForm(request.POST)
+        if form_instance.is_valid():
+            res=form_instance.save(commit=False)
+            res.role="instructor"
+            res.is_superuser=True
+            res.is_staff=True
+            res.password=make_password(form_instance.cleaned_data.get("password"))
+            form_instance.save()
+            return HttpResponse("data added")
+
--- django/e-learning/learnproject/learnproject/urls.py
+++ django/e-learning/learnproject/learnproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from instructorapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('instructor/reg',views.InstructorView.as_view(),name='instructorreg')
 ]
