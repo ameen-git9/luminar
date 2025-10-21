b67715ce3adf9e0ffb6df1b65722f53ce89eec98
ameen-git9 <ameenanu890@gmail.com>
2025-10-21 12:03:22 +0530

    instructorapp

--- django/e-learning/learnproject/instructorapp/models.py
+++ django/e-learning/learnproject/instructorapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import signals
 
 # Create your models here.
 
@@ -10,3 +11,20 @@ class User(AbstractUser):
         ('student','student')
     )
     role=models.CharField(max_length=100,choices=option,default="student")
+
+
+
+class InstructorProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="instructor")
+    profile_pic=models.ImageField(upload_to="profile_pic",null=True,blank=True,default="profile_pic/profile.jpg")
+    expertise=models.CharField(max_length=100,null=True,blank=True)
+    description=models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.user.username 
+    
+
+def create_profile(sender,instance,created,**kwargs):
+    if created and instance.role=="instructor":
+        InstructorProfile.objects.create(user=instance)
+signals.post_save.connect(create_profile,User)
