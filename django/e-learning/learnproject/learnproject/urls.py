e3af585f88023ab9865e5fd1ec500e6426a57661
ameen-git9 <ameenanu890@gmail.com>
2025-10-24 12:39:03 +0530

    course details added,like module,lessons.coursedetails.html  created

--- django/e-learning/learnproject/learnproject/urls.py
+++ django/e-learning/learnproject/learnproject/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from instructorapp import views
+from studentapp import views as studView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('instructor/reg',views.InstructorView.as_view(),name='instructorreg')
-]
+    path('instructor/reg',views.InstructorView.as_view(),name='instructorreg'),
+    path('student/home',studView.StudentHome.as_view(),name='studenthome'),
+    path('course/details/<int:id>',studView.CourseDetails.as_view(),name='coursedetails'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/e-learning/learnproject/studentapp/views.py
+++ django/e-learning/learnproject/studentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views import View
+from instructorapp.models import Course,Category
 # Create your views here.
+
+
+class StudentHome(View):
+    def get(self,request):
+       
+        courses=Course.objects.all()
+        return render(request,"student_home.html",{'courses':courses})
+
+
+class CourseDetails(View):
+    def get(self,request,**kwargs):
+        course=Course.objects.get(id=kwargs.get("id"))
+        return render(request,'course_details.html',{'course':course})
