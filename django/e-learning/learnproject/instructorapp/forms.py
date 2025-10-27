55fff6b6aff92b61c74052e7275c333f049d7ef4
ameen-git9 <ameenanu890@gmail.com>
2025-10-27 12:40:49 +0530

    register annd login

--- django/e-learning/learnproject/instructorapp/forms.py
+++ django/e-learning/learnproject/instructorapp/forms.py
@@ -7,7 +7,7 @@ class InstructorCreateForm(forms.ModelForm):
         model=User
         fields=['first_name','username','email','password']
         widgets={
-            'first_name':forms.TextInput(attrs={'class':'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent'}),
+            'first_name':forms.TextInput(attrs={'class':'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent '}),
             'username':forms.TextInput(attrs={'class':'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent'}),
             'email':forms.EmailInput(attrs={'class':'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent'}),
             'password':forms.PasswordInput(attrs={'class':'w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent'}),
--- django/e-learning/learnproject/studentapp/views.py
+++ django/e-learning/learnproject/studentapp/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
-from instructorapp.models import Course,Category
+from instructorapp.models import Course,Category,User
 from instructorapp.forms import InstructorCreateForm
+# from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 
 class StudentReg(View):
     def get(self,request):
-        form=InstructorCreateForm()
-        return render (request,'student_register.html',{'form':form})
+       
+        return render (request,'student_register.html')
+    
+    def post(self,request):
+        fname=request.POST.get('first_name')
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        print(fname,uname,email,password)
+        try:
+            User.objects.create_user(first_name=fname,username=uname,email=email,password=password)
+            messages.warning(request,'Registration successfull')
+            return redirect('studentlogin')
+        except:
+            messages.warning(request,'Invalid inputs')
+            return redirect('studentregister')
+        
+
+    
+
+
+class StudentLogin(View):
+    def get(self,request):
+        return render(request,'student_register.html')
+    
+    def post(self,request):
+        uname=request.POST.get('username')
+        password=request.POST.get('password')
+        res=authenticate(request,username=uname,password=password)
+        if res:
+            login(request,res)
+            if res.role=='student':
+                return redirect('studenthome')
+
+
 
 class StudentHome(View):
     def get(self,request):
