64c3f93e1c8916556954d56746625ca9129c40e4
ameen-git9 <ameenanu890@gmail.com>
2025-11-14 11:32:56 +0530

    models.py added

--- django_REST/TODO/server/todop_project/todo_app/models.py
+++ django_REST/TODO/server/todop_project/todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Todo(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField()
+    description=models.TextField()
+    added_date=models.DateField(auto_now_add=True)
+    due_date=models.DateField()
+    status=models.CharField(max_length=150,default="pending")
--- django_REST/TODO/server/todop_project/todo_app/serializer.py
+++ django_REST/TODO/server/todop_project/todo_app/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from todo_app.models import Todo
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['username','email','password']
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    id=serializers.IntegerField(read_only=True)
+    user=serializers.CharField(read_only=True)
+    class Meta:
+        model=Todo
+        fields='__all__'
