69a4520d56e2c455a41c5cddb6753eb4ca2d9f99
ameen-git9 <ameenanu890@gmail.com>
2025-11-26 12:34:28 +0530

    keywords let, var, const

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,37 @@
-console.log("ameen");
+// console.log("ameen");
+// s='hello'
+// d="hello"
+// f='javascript'
+// console.log(typeof d);
+
+// keywords :- let = local, var = global, const = local
+
+
+// let x=55
+// {
+//     let x=60
+//     console.log("inside:",x);
+    
+// }
+// console.log("outside:",x);
+
+
+// var x=105
+// {
+//     var x=205
+//     console.log("inside:",x);
+    
+// }
+// console.log("outside:",x);
+
+
+// const x=55
+// {
+//     const x=60
+//     console.log("inside:",x);
+    
+// }
+// console.log("outside:",x);
+
+
+const pi = 3.14
