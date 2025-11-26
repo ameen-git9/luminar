5d25eedfb28cb0892fe8f3605476db0990269c44
ameen-git9 <ameenanu890@gmail.com>
2025-11-26 13:05:04 +0530

    data types

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@
 // d="hello"
 // f='javascript'
 // console.log(typeof d);
+// -----------------------------------------------------------------------------
 
 // keywords :- let = local, var = global, const = local
 
@@ -24,6 +25,9 @@
 // }
 // console.log("outside:",x);
 
+// o/p :- inside: 205
+//        outside: 205
+
 
 // const x=55
 // {
@@ -34,4 +38,86 @@
 // console.log("outside:",x);
 
 
-const pi = 3.14
+// const pi = 3.14
+
+// ---------------------------------------------------------------------
+
+// num= 10.55
+// console.log(typeof num);
+
+// o/p :- number
+
+
+// num= 10
+// console.log(typeof num);
+
+// o/p :- number
+
+// ----------------------------------------------------------
+
+// DATA TYPES 
+// String, Boolean, Number
+
+// s='hello'
+// console.log(typeof s);
+// o/p :- String
+
+// num=10.3
+// console.log(typeof num);
+// o/p :- Number
+
+// b=false
+// console.log(typeof b);
+// o/p :-boolean
+
+// -------------------------------------------------------------------
+
+// let x
+// console.log(typeof x);  o/p :-undefined
+// console.log("hello"/2);  o/p :-NaN (not a number)
+
+
+// -----------------------------------------------------------
+
+// Object :- keyword and Array
+
+// array
+
+// arr1=[1,2,3,4,5]
+// console.log(arr1);         o/P :- [ 1, 2, 3, 4, 5 ]
+// console.log(typeof arr1);   o/p :- Object
+
+
+// person={name:"ameen",place:"calicut"}
+// console.log(person);              o/p :- { name: 'ameen', place: 'calicut' }
+// console.log(typeof person);       o/p :- Object
+
+
+// ------------------------------------------------------------
+
+//           operators
+
+// arithematic operator
+
+// let num1=10
+// console.log(num1++); // posr-increment   o/p :- 10
+
+// let num2=20
+// console.log(++num2);  //pre-increment    o/p :- 21
+
+
+
+// x=6
+// y=++x
+// console.log(x);  //o/p:- 7
+// console.log(y);  //o/p:- 7
+
+
+// x=6
+// y=x++
+// console.log(x);  //o/p:- 7
+// console.log(y);  // o/p:- 6
+
+
+
+
