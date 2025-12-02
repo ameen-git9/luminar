558d1d55bb51a299172f629f94936edf01caa547
ameen-git9 <ameenanu890@gmail.com>
2025-12-02 12:16:22 +0530

    array methods, arrow fuction, asynchronous, promise.

--- javascript/string_methods.js
+++ javascript/string_methods.js
@@ -53,11 +53,101 @@
 // console.log(s.concat(" ","hates"));    //out:- hello world hates
 
 
-s="hello"
-pattern=/l/i
-x=s.match(pattern)
-log
+// s="hello"
+// pattern=/l/i
+// x=s.match(pattern)
+// log
+
+// -----------------------------------------------------------------
+
+
+          // Array methods
+
+// arr1=[100,200,300,400,500]
+// console.log(arr1[0]);
+// // console.log(arr1);
+// console.log(arr1.pop());  // remove from end
+// // console.log(arr1);
+// console.log(arr1.shift());  // remove from start
+// console.log(arr1);
+// arr1.push(1000)    //add end
+// console.log(arr1);
+// arr1.unshift(1,2,3,4,5)   //add first
+// console.log(arr1);
+
+// console.log(delete arr1[1]);      // [ <1 empty item>, 200, 300, 400, 500 ] delete 
+// console.log(arr1);
+
+// console.log(arr1.slice(2,4));   //[ 300, 400 ]
+
+// arr1.splice(2,0,1000)     //[ 100, 200, 1000, 300, 400, 500 ]
+// console.log(arr1);
+
+// arr1.splice(2,2,1000)     //[ 100, 200, 1000, 400, 500 ]
+// console.log(arr1);
+
+
+// console.log(arr1.lenght);
+// console.log(arr1.at(0));
+
+
+// -----------------------------------------------------------
+
+
+                            // Arrow function - anonymous function
+
+
+// (arguments)=>(exp)
+// x=>()
+// y=>{}
+
+// function fun(){
+//     return "hello"
+// }
+
+// fun()
+
+// let f=(x,y)=>{
+//     console.log(x);
+//     console.log(y);
+
+//     return x+y
+    
+// }
+// console.log(f(2,3));
 
 
 
+
+                        // asynchronous
+
+
+// console.log("welcome");
+// setTimeout(()=>{
+//     console.log("asynchronous(timer)");
+    
+// },2000)
+
+// console.log("tata bye");
+
+
+
+//            promise -  fulfilled, pending, rejected
+
+let promise=new Promise(function(resolve,reject){     // anonymous function
+    let data=true
+    if(data){
+        resolve("fulfilled")
+    }
+    else{
+        reject("rejected")
+    }
+})
+
+promise.then((result)=>{   // arrow function
+    console.log(result);
+    
+}).catch((error)=>{
+    console.log(error);  
+})
 
